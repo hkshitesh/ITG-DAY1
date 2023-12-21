75c5a4a9cc1d47b519564ba7d1904a9119c06b4c
arjun negi <an096237@gmail.com>
2023-12-21 11:35:04 +0530

    add addition program

---
+++ src/main/java/mypkg/711340101002.java
@@ -0,0 +1,9 @@
+package mypkg;
+
+public class 711340101002 {
+    public static void main(String[] args) {
+        int a = 5, b = 8;
+        int c=a+b;
+        System.out.println("The sum of "+a+" and "+b+" is "+c);
+    }
+}
